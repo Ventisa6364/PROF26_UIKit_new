06ca92ce1935401b1bd2d23c30b81c56f1aac2f8
Ventisa6364 <cherkgosa@icloud.com>
2026-02-16 21:40:28 +0300

    добавление элементов UIKit на testpage

--- example/lib/presentation/pages/test_page.dart
+++ example/lib/presentation/pages/test_page.dart
@@ -1,10 +1,242 @@
 import 'package:flutter/material.dart';
+import 'package:prof26_uikit/prof26_uikit.dart';
 
 class TestPage extends StatelessWidget {
-  const TestPage({super.key});
+  TestPage({super.key});
+
+  final _lightPalette = LightPalette();
+  final _darkPalette = DarkPalette();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var theme = CustomTheme.of(context);
+    return Scaffold(
+      backgroundColor: theme.palette.background,
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: MediaQuery.paddingOf(context),
+          child: Column(
+            crossAxisAlignment: .center,
+            mainAxisAlignment: .start,
+            children: [
+              Row(
+                mainAxisAlignment: .spaceEvenly,
+                crossAxisAlignment: .start,
+                children: [
+                  Column(
+                    children: [
+                      Text(_lightPalette.runtimeType.toString()),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.background,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.black,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.block,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.button,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.buttonInactive,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.border,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.icon,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.text,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.textSecondary,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.hint,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.accent,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _lightPalette.error,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(_darkPalette.runtimeType.toString()),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.background,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.black,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.block,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.button,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.buttonInactive,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.border,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.icon,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.text,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.textSecondary,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.hint,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.accent,
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        color: _darkPalette.error,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(theme.style.runtimeType.toString()),
+                  Text(
+                    'Nunito Bold 32',
+                    style: theme.style.nunitoBold32.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Medium 24',
+                    style: theme.style.nunitoMedium24.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Medium 22',
+                    style: theme.style.nunitoMedium22.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito ExtraLight 20',
+                    style: theme.style.nunitoExtraLight20.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Medium 18',
+                    style: theme.style.nunitoMedium18.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Light 18',
+                    style: theme.style.nunitoLight18.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Medium 16',
+                    style: theme.style.nunitoMedium16.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Light 16',
+                    style: theme.style.nunitoLight16.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Medium 14',
+                    style: theme.style.nunitoMedium14.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Light 14',
+                    style: theme.style.nunitoLight14.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Regular 12',
+                    style: theme.style.nunitoRegular12.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                  Text(
+                    'Nunito Medium 11',
+                    style: theme.style.nunitoMedium11.copyWith(
+                      color: theme.palette.text,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
