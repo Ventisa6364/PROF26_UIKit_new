58eaa87a1abd645c8f02bac5609e4e698ea9a436
Ventisa6364 <cherkgosa@icloud.com>
2026-02-16 20:30:15 +0300

    добавление styles и palette

---
+++ lib/src/presentation/theme/styles.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Styles {
+  String get _nunitoFamily => "Nunito";
+
+  String get _package => 'prof26_uikit';
+
+  TextStyle get nunitoBold32 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w700,
+    fontSize: 32.sp,
+  );
+
+  TextStyle get nunitoMedium24 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w500,
+    fontSize: 24.sp,
+  );
+
+  TextStyle get nunitoMedium22 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w500,
+    fontSize: 22.sp,
+  );
+
+  TextStyle get nunitoExtraLight20 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w200,
+    fontSize: 20.sp,
+  );
+
+  TextStyle get nunitoLight18 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w300,
+    fontSize: 18.sp,
+  );
+
+  TextStyle get nunitoMedium18 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w500,
+    fontSize: 18.sp,
+  );
+
+  TextStyle get nunitoMedium16 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+  );
+
+  TextStyle get nunitoLight16 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w300,
+    fontSize: 16.sp,
+  );
+
+  TextStyle get nunitoMedium14 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w500,
+    fontSize: 14.sp,
+  );
+
+  TextStyle get nunitoLight14 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w300,
+    fontSize: 14.sp,
+  );
+
+  TextStyle get nunitoRegular12 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w400,
+    fontSize: 12.sp,
+  );
+
+  TextStyle get nunitoMedium11 => TextStyle(
+    fontFamily: _nunitoFamily,
+    package: _package,
+    fontWeight: FontWeight.w500,
+    fontSize: 11.sp,
+  );
+}
